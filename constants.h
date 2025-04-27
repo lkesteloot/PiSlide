b3e94ae22912ef4f7744df945e2577fa31cbd4fa
Lawrence Kesteloot <lk@teamten.com>
2025-04-26 22:05:42 -0700

    Switch to fast speed during development.

--- constants.h
+++ constants.h
@@ -2,7 +2,7 @@
 #pragma once
 
 // For debugging, should normally be False:
-constexpr bool QUICK_SPEED = false;
+constexpr bool QUICK_SPEED = true;
 
 // The display time includes only one transition (the end one):
 constexpr float SLIDE_DISPLAY_S = QUICK_SPEED ? 5 : 14;
